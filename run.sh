500d5c525cb22d2b877d443bdb1e7690abe19331
Oak Chen <oak@sfysoft.com>
2019-12-24 13:06:50 +0800

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,52 @@
+#! /bin/sh
+
+# buildenv docker运行脚本
+# Copyright (C) 2019-2019 Oak Chen <oak@sfysoft.com>
+
+until [ $# -eq 0 ]
+do
+	case $1 in
+	-h | --help)
+		echo "Usage:"
+		echo "-h, --help        Display this information"
+		echo "-n, --no-home     Don't map $HOME directory"
+		echo "-p, --passwd      Map /etc/passwd"
+		echo "-g, --group       Map /etc/group"
+		exit
+		;;
+	-n | --no-home)
+		nohome=true
+		;;
+	-p | --password)
+		mappasswd=true
+		;;
+	-g | --group)
+		mapgroup=true
+		;;
+	*)
+		others="$*"
+		break
+		;;
+	esac
+	shift
+done
+
+
+if [ -z "$nohome" ]; then
+	options="$options -v $HOME:$HOME"
+fi
+
+if [ "$mappasswd" = "true" ]; then
+	options="$options -v /etc/passwd:/etc/passwd"
+fi
+
+if [ "$mapgroup" = "true" ]; then
+	options="$options -v /etc/group:/etc/group"
+fi
+
+options="$options $others"
+
+commandline="docker run -it $options oakchen/buildenv bash"
+
+echo $commandline
+eval $commandline
